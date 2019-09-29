f36ae430e45702201260132936a5674c0efc46d5
spriglet <redfroglet@gmail.com>
2019-09-29 13:54:47 +0900

    Delete two files that where not part of the template

--- src/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from "./reducers/";
-export const store = createStore(rootReducer, composeWithDevTools(
-  applyMiddleware(...applyMiddleware),
-  // other store enhancers if any
-));
